e19f1413703dfea59e01d5090296af4f1027fe8b
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-13 14:01:50 +0600

    set icon and api bug fixes done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const petContainer = document.getElementById('petContainer');
 
 
-
+const petCardContainer = document.getElementById("petCardContainer");
 
 
 const loadPetCategory = () => {
@@ -17,24 +17,78 @@ const loadPetCategory = () => {
     })
 }
 
-
-
 const showPetCategory = (categories) => {
+    console.log(categories);
     categories.forEach((category) => {
         petContainer.innerHTML += `
-        <li onclick = 'clickCategory(${category.id})' id='${category.id}' class="border border-green-100 hover:bg-[#0E7A81] mb-4 hover:text-white cursor-pointer px-7 py-3  rounded-xl "><i class=" ${category.category_icon}"></i>
+        <div class="flex justify-center items-center border border-green-100 hover:bg-[#0E7A81] mb-4 hover:text-white cursor-pointer px-7 py-3  rounded-xl">
+        <img src="${category.category_icon}">
+        <li onclick = 'loadPetsByCategory("${category.category}")' id='${category.id}'>
         ${category.category}
         </li>
-        
+        </div>
         `
     });
+
+    // petContainer.addEventListener('click', (e) => {
+    //     const allLi = document.querySelectorAll("li");
+    //     allLi.forEach((li) => {
+    //         li.classList.remove("bg-[#0E7A81]", "text-white")
+    //     })
+    //     if(e.target.category === 'li'){
+    //         e.target.classList.add("bg-[#0E7A81]", "text-white");
+    //     }
+
+    // })
 }
 
 
 
+// const loadPetDetails = async (id) => {
+//     const ulr = `https://openapi.programming-hero.com/api/peddy/pets/${id}`
+//     console.log(ulr)
+//     const res = await fetch(ulr);
+//     const data = await res.json();
+//     console.log(data.pets)
+//     petsDisplayDetails(data.pets)
+// };
+
+// const petsDisplayDetails = (pets) => {
+//     console.log(pets)
+
+//     // const petDetailsCardBox = document.getElementById("")
+// }
+
+const loadPetsByCategory = (petId) => {
+    console.log(petId)
+    const url = petId ? `https://openapi.programming-hero.com/api/peddy/category/${petId.toLowerCase()}` : `
+    https://openapi.programming-hero.com/api/peddy/pets`
+
+
+    fetch(url)
+    .then((res) => {
+        return res.json()})
+    .then((data) => {
+        console.log(data)
+        const pets = data.pets ? data.pets : data.data
+        showPetsByCategory(pets)
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
+}
+
+
+const showPetsByCategory = (pets) => {
+    petCardContainer.innerHTML = " ";
+    pets.forEach((pet) => {
+        petCardContainer.innerHTML += `
+        
+        `
+    })
+}
 
 
 
-
-
+loadPetsByCategory();
 loadPetCategory();
